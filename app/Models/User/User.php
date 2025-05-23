28de13d901d2dbc92e8bee4ae55943eb629bf7b4
Mathias Eudeline <mathias.eudeline.ext@groupama-ra.fr>
2025-05-23 14:40:02 +0200

    feat: install & setup laravel-permission

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -1,16 +1,17 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\User;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $admin = Role::query()->firstOrCreate(['name' => 'admin', 'guard_name' => 'api']);
+
+        $admin->syncPermissions([
+            Permission::query()->firstOrCreate(['name' => 'profile.store', 'guard_name' => 'api']),
+            Permission::query()->firstOrCreate(['name' => 'profile.update', 'guard_name' => 'api']),
+            Permission::query()->firstOrCreate(['name' => 'profile.delete', 'guard_name' => 'api']),
+            Permission::query()->firstOrCreate(['name' => 'profile.comment.store', 'guard_name' => 'api']),
+        ]);
+    }
+}
